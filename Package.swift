ffcb4e1e73ae8512759898aeea90884c09d92e99
Alexey Ivashko <a_ivashko@apple.com>
2024-04-04 10:41:19 -0700

    Adopt strict concurrency for Package
    * Update swift-package-version to 5.9 to leverage `enableUpcomingFeature`
    * Fix some of the warnings that were related to not marking `SecAccessPolicy` types as Sendable

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,8 @@ let package = Package(
     targets: [
         .target(
             name: "SwiftSecurity",
-            dependencies: []
+            dependencies: [],
+            swiftSettings: [.enableUpcomingFeature("StrictConcurrency")]
         ),
         .testTarget(
             name: "SwiftSecurityTests",
