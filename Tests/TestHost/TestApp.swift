047a63f6e7f49daa966c15d075ba4dea0c65bfe6
Dmitriy Zharov <contact@zharov.dev>
2024-04-29 16:31:14 +0700

    Destinations "watchOS, visionOS (Designed for iPad), Mac (Mac Catalyst)" for TestHost

---
+++ Tests/TestHost/TestApp.swift
@@ -0,0 +1,15 @@
+//
+//  TestApp.swift
+//  TestHost
+//
+//  Created by Dmitriy Zharov on 29.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup { }
+    }
+}
